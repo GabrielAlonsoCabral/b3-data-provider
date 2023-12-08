9ea5e4e9cf400e194d531800373c63536f470e1d
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2023-12-07 22:07:13 -0300

    [IMPROVEMENT] - b3-data-provider - master

---
+++ scripts/pgdata_backup.sh
@@ -0,0 +1,19 @@
+
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+
+echo "INFO:  SH SCRIPT $PWD/scripts/pgdata_backup.sh STARTED..."
+
+readonly DUMP_NAME="DUMP_$POSTGRES_DB-$(date +%m-%d-%YT%H:%M:%S)"
+
+echo "\n"
+echo "INFO: EXECUTING BACKUP ON DATABASE $POSTGRES_DB ... \n"
+docker exec -i $POSTGRES_CONTAINER_NAME /bin/bash -c "PGPASSWORD=$POSTGRES_PASSWORD pg_dump --username $POSTGRES_USER ${POSTGRES_DB}" > $DUMP_NAME.sql;
+echo "INFO: DUMP $DUMP_NAME.sql generated. \n"
+
+echo "INFO: UPLOADING DUMP TO S3 $DUMP_NAME.sql ...\n"
+aws s3 mv "$DUMP_NAME".sql s3://$AWS_BUCKET_NAME/pgdata_dumpies/$DUMP_NAME.sql > ./.logs/$DUMP_NAME.txt
+echo "INFO:  LOGS CREATED ON $PWD/.logs/$DUMP_NAME.txt"
+echo "INFO:  BACKUP FINISHED!\n"
